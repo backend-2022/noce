5eca6a99bae7929f91d4f0481ab1309357dfd46a
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-12-17 14:12:00 +0200

    add utms

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\Website\FreeDesignController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('capture.utm')->group(function () {
-    Route::get('/', [HomeController::class, 'index'])->name('home');
-    Route::post('/free-design', [FreeDesignController::class, 'store'])->name('free-design.store');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::post('/free-design', [FreeDesignController::class, 'store'])->name('free-design.store');
 });
